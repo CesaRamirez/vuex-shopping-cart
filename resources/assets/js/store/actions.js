b62700edf75f143168b83f49de54099db90f7a92
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-12-21 20:11:26 -0600

    Update dependencies and remove hard code url

--- resources/assets/js/store/actions.js
+++ resources/assets/js/store/actions.js
@@ -1,7 +1,7 @@
 export const fetchNumber = ({
     commit
 }, {min, max}) => axios
-    .get(`http://vuex-shopping-cart.dev/api/random/${min}/${max}`)
+    .get(`/api/random/${min}/${max}`)
     .then((response) => {
         commit('addRandomNumber', response.data.number)
     })
